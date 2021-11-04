46d201f344af337f13e5701e856062eb469e636c
mishan010 <93714311+mishan010@users.noreply.github.com>
2021-11-04 18:41:59 +0600

    Update showOnTermux.sh

--- showOnTermux.sh
+++ showOnTermux.sh
@@ -14,7 +14,7 @@ echo "
    < ━━━━━━━━━━━ [★]@mishan010[★] ━━━━━━━━━━━━ > " |lolcat
 
 
-python /data/data/com.termux/files/usr/etc/wlc.py
+python /data/data/com.termux/files/usr/etc/showOnTermux.py
 if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
         command_not_found_handle() {
                 /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
